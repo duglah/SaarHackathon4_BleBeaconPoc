f2bd1c3a0ffa1d9cf32c69a04abf1d935b4a3a38
Philipp Manstein <philipp.gm@googlemail.com>
2023-10-15 13:59:20 +0200

    Using EddyStone Sample advertising

--- src/bluetooth.hpp
+++ src/bluetooth.hpp
@@ -9,6 +9,7 @@ namespace BLE
 {
     // #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
     static BLEUUID SERVICE_UUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+    uint16_t beconUUID = 0xFEAA;
 
     TaskHandle_t task1;
     BLEAdvertising *pAdvertising;
@@ -28,19 +29,20 @@ namespace BLE
                 v.push_back(device);
 
                 auto payload = device.getPayload();
-                auto serviceData = device.getServiceData();
                 auto payloadLength = device.getPayloadLength();
 
-                char str[payloadLength + 1];
-                // memcpy(str, payload, payloadLength);
-                // for(int i = 0; i < payloadLength; payloadLength++) {
-                //     str[i] = payload[10 + i];
-                // }
-                str[payloadLength] = '\0';
+                if (payload[11] == 0x20)
+                {
+                    std::string eddyContent = "1234";
 
-                Serial.printf("Payload: %s\n", str);
-                Serial.printf("Payload length: %d\n", payloadLength);
-                Serial.printf("ServiceData: %s\n", serviceData);
+                    for (int idx = 0; idx < 4; idx++)
+                    {
+                        eddyContent[idx] = payload[idx + 11];
+                    }
+
+                    Serial.printf("Payload: %s\n", eddyContent);
+                    Serial.printf("Payload length: %d\n", payloadLength);
+                }
             }
             auto callback = *static_cast<std::function<void(std::vector<BLEAdvertisedDevice>)> *>(pvParameters);
             callback(v);
@@ -52,37 +54,49 @@ namespace BLE
     void setup(T *callback, long device_id)
     {
         char buf[25];
-        snprintf(buf, sizeof(buf), "St Martins Laterne %lu", device_id);
-        BLEDevice::init(buf);
-        // BLEDevice::init("St Martins Laterne");
-        BLEServer *pServer = BLEDevice::createServer();
-        BLEService *pService = pServer->createService(SERVICE_UUID);
+        // snprintf(buf, sizeof(buf), "St Martins Laterne %lu", device_id);
+        // BLEDevice::init(buf);
+        BLEDevice::init("TLMBeacon");
+        BLEDevice::setPower(ESP_PWR_LVL_N12);
+        // BLEServer *pServer = BLEDevice::createServer();
+        // BLEService *pService = pServer->createService(SERVICE_UUID);
         // BLECharacteristic *pCharacteristic = pService->createCharacteristic(
         //     CHARACTERISTIC_UUID,
         //     BLECharacteristic::PROPERTY_READ);
 
         // pCharacteristic->setValue("Hello World");
-        pService->start();
+        // pService->start();
         pAdvertising = BLEDevice::getAdvertising();
-        pAdvertising->setScanResponse(true);
-        pAdvertising->addServiceUUID(SERVICE_UUID);
-        pAdvertising->setScanResponse(true);
-        pAdvertising->setMinPreferred(0x06);
-        pAdvertising->setMinPreferred(0x12);
+        // pAdvertising->setScanResponse(true);
+        // pAdvertising->addServiceUUID(SERVICE_UUID);
+        // pAdvertising->setScanResponse(true);
+        // pAdvertising->setMinPreferred(0x06);
+        // pAdvertising->setMinPreferred(0x12);
 
         oScanResponseData.setFlags(0x06); // GENERAL_DISC_MODE 0x02 | BR_EDR_NOT_SUPPORTED 0x04
-        oScanResponseData.setCompleteServices(SERVICE_UUID);
-        oScanResponseData.setServiceData(SERVICE_UUID, "Test123");
+        oScanResponseData.setCompleteServices(BLEUUID(beconUUID));
+
+        char beacon_data[6];
+        beacon_data[0] = 0x20; // Eddystone Frame Type (Unencrypted Eddystone-TLM)
+        beacon_data[1] = 0x00; // TLM version
+        beacon_data[2] = 'T';
+        beacon_data[3] = 'e';
+        beacon_data[4] = 's';
+        beacon_data[5] = 't';
+
+        oScanResponseData.setServiceData(BLEUUID(beconUUID), std::string(beacon_data, 6));
+        // oScanResponseData.setServiceData(SERVICE_UUID, "Test123");
         oAdvertisementData.setName("TLMBeacon");
         pAdvertising->setAdvertisementData(oAdvertisementData);
         pAdvertising->setScanResponseData(oScanResponseData);
 
-        BLEDevice::startAdvertising();
+        // BLEDevice::startAdvertising();
+        pAdvertising->start();
 
         BLEScan *pBLEScan = BLEDevice::getScan();
+        pBLEScan->setActiveScan(true);
         pBLEScan->setInterval(1349);
         pBLEScan->setWindow(449);
-        pBLEScan->setActiveScan(true);
 
         xTaskCreatePinnedToCore(
             &Task1code, /* Task function. */
